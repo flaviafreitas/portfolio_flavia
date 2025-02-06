fcd65b89a2027beb9114278326fc8e50c3605219
Flávia Freitas <flaviarobertasf@gmail.com>
2025-02-05 23:56:07 -0300

    Add swiper in skills section

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,26 +1,33 @@
 export function initSwiper() {
-    const swiper = new Swiper('.swiper-container', {
+    const swiperProjects = new Swiper('.swiper-container', {
         loop: true,
         spaceBetween: 10,
         slidesPerView: 1,
         pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        type: 'bullets',
+            el: '.swiper-pagination',
+            clickable: true,
+            type: 'bullets',
         },
         breakpoints: {
-        768: {
-            slidesPerView: 1,
-            spaceBetween: 10,
+            768: { slidesPerView: 1, spaceBetween: 10 },
+            992: { slidesPerView: 2, spaceBetween: 20 },
+            1200: { slidesPerView: 3, spaceBetween: 30 },
         },
-        992: {
-            slidesPerView: 2,
-            spaceBetween: 20,
+    });
+
+    const swiperSkills = new Swiper('.swiper-skills', { 
+        loop: true,
+        spaceBetween: 20,
+        slidesPerView: 'auto',
+        speed: 3000,
+        freeMode: true,
+        autoplay: {
+            delay: 0,
         },
-        1200: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-        },
+        breakpoints: {
+            768: { slidesPerView: 3 },
+            992: { slidesPerView: 4 },
+            1200: { slidesPerView: 6 },
         },
     });
 }
